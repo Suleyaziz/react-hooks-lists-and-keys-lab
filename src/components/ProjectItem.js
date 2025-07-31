84897790d80f9f79d3e1bb0073bc71bcecdf60b4
Suleiman Aziz <Suleimanaziz212@gmail.com>
2025-07-31 22:42:34 +0300

    Lab complete

--- src/components/ProjectItem.js
+++ src/components/ProjectItem.js
@@ -6,8 +6,10 @@ function ProjectItem({ name, about, technologies }) {
       <h3>{name}</h3>
       <p>{about}</p>
       <div className="technologies">
-        {/* render a <span> for each technology in the technologies array */}
-      </div>
+        {technologies.map(technologies =>{
+          return <span key={technologies}>{technologies}</span>
+        })}
+     </div>
     </div>
   );
 }
